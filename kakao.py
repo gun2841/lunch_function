2652ea118762c9c3175eac9439e80b3046d728fa
gunho <gun2841@naver.com>
2022-06-15 13:09:03 +0900

    update menu

---
+++ kakao.py
@@ -0,0 +1,146 @@
+import collections
+import requests
+import pandas as pd
+import numpy as np
+import folium
+from folium.plugins import MiniMap
+import requests
+import folium
+import openpyxl
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+import csv
+import urllib.request
+import urllib.parse
+import re
+from bs4 import BeautifulSoup
+import platform
+from tqdm import tqdm
+from time import sleep
+from tqdm import trange
+from haversine import haversine
+def whole_region(keyword, start_x, start_y, end_x, end_y):
+    # print(start_x,start_y,end_x,end_y)
+    page_num = 1
+    # 데이터가 담길 리스트
+    all_data_list = []
+    while (1):
+        url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
+        params = {'query': keyword, 'page': page_num,'category_group_code':'FD6',
+                  'rect': f'{start_x},{start_y},{end_x},{end_y}'}
+        headers = {"Authorization": "KakaoAK 15c832307cafb72e89dbe1990cb85584"}
+        ## 입력예시 -->> headers = {"Authorization": "KakaoAK f64acbasdfasdfasf70e4f52f737760657"}
+        resp = requests.get(url, params=params, headers=headers)
+        search_count = resp.json()['meta']['total_count']
+        #print('총 개수', search_count)
+        if search_count > 45:
+            #print('좌표 4등분')
+            dividing_x = (start_x + end_x) / 2
+            dividing_y = (start_y + end_y) / 2
+            ## 4등분 중 왼쪽 아래
+            all_data_list.extend(whole_region(keyword, start_x, start_y, dividing_x, dividing_y))
+            ## 4등분 중 오른쪽 아래
+            all_data_list.extend(whole_region(keyword, dividing_x, start_y, end_x, dividing_y))
+            ## 4등분 중 왼쪽 위
+            all_data_list.extend(whole_region(keyword, start_x, dividing_y, dividing_x, end_y))
+            ## 4등분 중 오른쪽 위
+            all_data_list.extend(whole_region(keyword, dividing_x, dividing_y, end_x, end_y))
+            return all_data_list
+        else:
+            if resp.json()['meta']['is_end']:
+                all_data_list.extend(resp.json()['documents'])
+                return all_data_list
+            # 아니면 다음 페이지로 넘어가서 데이터 저장
+            else:
+                #print('다음페이지')
+                page_num += 1
+                all_data_list.extend(resp.json()['documents'])
+def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y):
+    # 최종 데이터가 담길 리스트
+    overlapped_result = []
+
+    # 지도를 사각형으로 나누면서 데이터 받아옴
+    for i in trange(1, num_x + 1):  ## 1,10
+        end_x = start_x + next_x
+        initial_start_y = start_y
+        for j in range(1, num_y + 1):  ## 1,6
+            end_y = initial_start_y + next_y
+            each_result = whole_region(keyword, start_x, initial_start_y, end_x, end_y)
+            overlapped_result.extend(each_result)
+            initial_start_y = end_y
+        start_x = end_x
+
+    return overlapped_result
+def make_map(dfs,loc_y,loc_x):
+    # 지도 생성하기
+    m = folium.Map(location=[loc_y,loc_x],
+                   zoom_start=12)
+
+    # 미니맵 추가
+    minimap = MiniMap()
+    m.add_child(minimap)
+
+    # 마커
+    for i in range(len(dfs)):
+        #l = haversine((loc_y, loc_x), (float(df['Y'][i]), float(df['X'][i])), unit='m')
+        folium.Marker([dfs['Y'][i],dfs['X'][i]],
+                  tooltip=dfs['stores'][i],
+                  popup=dfs['place_url'][i],
+                  ).add_to(m)
+    m.save('./test2.html')
+    return m
+#################### 메뉴 가져오기
+def _getMenuInfo(menu):
+    menuName = menu.select('.info_menu > .loss_word')[0].text
+    menuPrices = menu.select('.info_menu > .price_menu')
+    menuPrice = ''
+
+    if len(menuPrices) != 0:
+        menuPrice =  menuPrices[0].text.split(' ')[1]
+
+    return [menuName, menuPrice]
+def get_menu(ID,headless=True):
+    data=[]
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.151 Whale/3.14.134.62 Safari/537.36"}
+    url = "https://place.map.kakao.com/"
+    options = webdriver.ChromeOptions()
+    options.add_argument('lang=ko_KR')
+    if headless:
+        options.add_argument('headless')
+    chromedriver_path = ""
+    if platform.system() == "Windows":
+        chromedriver_path = "./chromedriver.exe"
+    elif platform.system() == "Linux":
+        chromedriver_path = "./chromedriver"
+    driver = webdriver.Chrome(os.path.join(os.getcwd(), chromedriver_path), options=options)
+    for id in tqdm(ID):
+        try:
+            sleep(0.2)
+            driver.get(url + id)
+            sleep(0.6)
+            menuInfos = []
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            sleep(1)
+
+            menuonlyType = soup.select('.cont_menu > .list_menu > .menuonly_type')
+            nophotoType = soup.select('.cont_menu > .list_menu > .nophoto_type')
+            photoType = soup.select('.cont_menu > .list_menu > .photo_type')
+            if len(menuonlyType) != 0:
+                for menu in menuonlyType:
+                    menuInfos.append(_getMenuInfo(menu))
+            elif len(nophotoType) != 0:
+                for menu in nophotoType:
+                    menuInfos.append(_getMenuInfo(menu))
+            else:
+                for menu in photoType:
+                    menuInfos.append(_getMenuInfo(menu))
+            data.append([id,menuInfos])
+        except:
+            pass
+    driver.close()
+    return data
+
--- main.py
+++ main.py
@@ -1,144 +1,43 @@
-import os
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementNotInteractableException
-from bs4 import BeautifulSoup
+import collections
+from kakao import overlapped_data,make_map,get_menu
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
-options = webdriver.ChromeOptions()
-#options.add_argument('headless')
-options.add_argument('lang=ko_KR')
-chromedriver_path = "./chromedriver"
-driver = webdriver.Chrome(os.path.join(os.getcwd(), chromedriver_path), options=options)  # chromedriver 열기
-
-def main():
-    global driver, menu_wb
-
-    driver.implicitly_wait(4)
-    driver.get('https://map.kakao.com/')
-
-    search("당산역 음식점")
-
-    driver.quit()
-    print("finish")
-
-
-def search(place):
-    global driver
-
-    search_area = driver.find_element_by_xpath('//*[@id="search.keyword.query"]')
-    search_area.send_keys(place)
-    driver.find_element_by_xpath('//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)
-    sleep(1)
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    search_area.clear()
-    # 1페이지 크롤링
-    place_lists = soup.select('.placelist > li')  # 장소 목록 list
-    crawling(place_lists)
-
-    # 더보기 클릭해서 2페이지
-    try:
-        driver.find_element_by_xpath('//*[@id="info.search.place.more"]').send_keys(Keys.ENTER)
-        sleep(1)
-        # 2 페이지 이후 크롤링 시작
-        while True:
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
-            search_page = soup.select("#info\.search\.page > .pageWrap > a")
-            sleep(1)
-            for i in range(1,len(search_page)+1):
-                xPath = '//*[@id="info.search.page.no' + str(i) + '"]'
-                driver.find_element_by_xpath(xPath).send_keys(Keys.ENTER)
-                place_lists = soup.select('.placelist > li')  # 장소 목록 list
-                crawling(place_lists)
-            next_page='//*[@id="info.search.page.next"]'
-            disable=soup.select('#info\.search\.page\.next')[0].attrs['class'][0]
-            if disable != "next":
-
-                break
-            driver.find_element_by_xpath(next_page).send_keys(Keys.ENTER)
-            sleep(1)
-    except ElementNotInteractableException:
-        print('not found')
-    finally:
-        search_area.clear()
-
-def crawling(placeLists):
-    for i, place in enumerate(placeLists):
-        find_tag = place.attrs['class'][0]
-        if find_tag=='AdItem':
-            print("find!!!!!!!!!!!!!!!!")
-        else:
-            #menuInfos = getMenuInfo(i, driver)
-            #print(menuInfos)
-            Info=getInfo(i,driver)
-            print(Info)
-
-def getInfo(i,driver):
-    detail_page_xpath = '//*[@id="info.search.place.list"]/li[' + str(i + 1) + ']/div[5]/div[4]/a[1]'
-    driver.find_element_by_xpath(detail_page_xpath).send_keys(Keys.ENTER)
-    driver.switch_to.window(driver.window_handles[-1])  # 상세정보 탭으로 변환
-    sleep(1)
-    Infos = []
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    sleep(1)
-    # mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > h2
-    # mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > h2
-    title = soup.select_one('#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > h2')
-    rate = soup.select_one('#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(3) > span.color_b')
-    #category = soup.find('#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > span.txt_location')[0].text
-    print(title)
-    Infos.append(title.text)
-    Infos.append(rate.text)
-    #Infos.append(category)
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])  # 검색 탭으로 전환
-    return Infos
-
-def getMenuInfo(i, driver):
-    # 상세페이지로 가서 메뉴찾기
-
-    detail_page_xpath = '//*[@id="info.search.place.list"]/li[' + str(i + 1) + ']/div[5]/div[4]/a[1]'
-    driver.find_element_by_xpath(detail_page_xpath).send_keys(Keys.ENTER)
-    driver.switch_to.window(driver.window_handles[-1])  # 상세정보 탭으로 변환
-    sleep(1)
-
-    menuInfos = []
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    # 메뉴의 3가지 타입
-    menuonlyType = soup.select('.cont_menu > .list_menu > .menuonly_type')
-    nophotoType = soup.select('.cont_menu > .list_menu > .nophoto_type')
-    photoType = soup.select('.cont_menu > .list_menu > .photo_type')
-
-    if len(menuonlyType) != 0:
-        for menu in menuonlyType:
-            menuInfos.append(_getMenuInfo(menu))
-    elif len(nophotoType) != 0:
-        for menu in nophotoType:
-            menuInfos.append(_getMenuInfo(menu))
-    else:
-        for menu in photoType:
-            menuInfos.append(_getMenuInfo(menu))
-
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])  # 검색 탭으로 전환
-
-    return menuInfos
-
-def _getMenuInfo(menu):
-    menuName = menu.select('.info_menu > .loss_word')[0].text
-    menuPrices = menu.select('.info_menu > .price_menu')
-    menuPrice = ''
-
-    if len(menuPrices) != 0:
-        menuPrice =  menuPrices[0].text.split(' ')[1]
-
-    return [menuName, menuPrice]
-
-if __name__ == "__main__":
-    main()
+loc_x , loc_y = 126.898242, 37.530774
+keyword = '음식점'
+# 시작 좌표
+start_x = 126.891
+start_y = 37.529
+next_x = 0.001
+next_y = 0.001
+num_x = 14
+num_y = 8
+print("검색 시작")
+overlapped_result = overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y)
+print("검색 종료")
+# 중복 제거
+results = list(map(dict, collections.OrderedDict.fromkeys(tuple(sorted(d.items())) for d in overlapped_result)))
+X = []
+Y = []
+stores = []
+road_address = []
+place_url = []
+ID = []
+category = []
+for place in results:
+    X.append(float(place['x']))
+    Y.append(float(place['y']))
+    stores.append(place['place_name'])
+    road_address.append(place['road_address_name'])
+    place_url.append(place['place_url'])
+    ID.append(place['id'])
+    category.append(place['category_name'])
+    ar = np.array([ID, stores, X, Y, category,road_address, place_url]).T
+    df = pd.DataFrame(ar, columns=['ID', 'stores', 'X', 'Y','category', 'road_address', 'place_url'])
+print('total_reuslt_number = ', len(df))
+print("메뉴가져오기 시작")
+menus = get_menu(ID)
+print("메뉴가져오기 종료")
+#make_map(df,loc_y,loc_x)
+df.to_excel("./menu.xlsx")
